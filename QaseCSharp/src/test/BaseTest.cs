e2a5c892a90fd17e27bdbc45c6e4b03755d9e6d5
Viktoryia.Latosh <latosh.viktorija@mail.ru>
2021-08-24 17:32:24 +0300

    Added login test

---
+++ QaseCSharp/src/test/BaseTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace QaseCSharp
+{
+    public class BaseTest
+    {
+        protected IWebDriver Driver;
+        protected string Email;
+        protected string Password;
+        protected string BaseUrl;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            DotNetEnv.Env.TraversePath().Load();
+            Email = DotNetEnv.Env.GetString("QASE_EMAIL", "Email not found");
+            Password = DotNetEnv.Env.GetString("QASE_PASSWORD", "Password not found");
+            BaseUrl = DotNetEnv.Env.GetString("QASE_BASE_URL", "Url not found");
+            var options = new ChromeOptions();
+            options.AddArguments("--start-maximized");
+            options.AddArguments("--headless");
+            options.AddArguments("--disable-notifications");
+            Driver = new ChromeDriver(options);
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            Driver.Close();
+        }
+    }
+}
